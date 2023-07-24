fde7177929a48ea7881526a2e3ee2caaa63f32b5
v.konovalov <v.konovalov@iskrauraltel.ru>
2023-07-24 09:47:05 +0500

    ДЗ-1 Проект gradle с модульной структурой. Дополнение. Подключение sonarlint и spotless плагинов

--- hw01-gradle/src/main/java/ru/otus/HelloOtus.java
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -2,6 +2,7 @@ package ru.otus;
 
 import com.google.common.collect.Lists;
 
+@SuppressWarnings("java:S106")
 public class HelloOtus {
     public static void main(String... args) {
         var numbers = Lists.newArrayList(1, 2, 3, 4, 5);
