69fb06a8e487018bdf0dcd0e017d27c965a1704d
v.konovalov <v.konovalov@iskrauraltel.ru>
2023-10-24 18:30:17 +0500

    ДЗ-4 Определение нужного размера хипа

---
+++ hw04-gc/src/main/java/ru/otus/hw04/DataOpt.java
@@ -0,0 +1,13 @@
+package ru.otus.hw04;
+
+public class DataOpt {
+    private final int value;
+
+    public DataOpt(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
