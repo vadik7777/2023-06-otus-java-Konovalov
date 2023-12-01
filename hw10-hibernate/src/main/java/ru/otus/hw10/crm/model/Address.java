2ba14f5f37409f6750dd0b74ec0e620dfca01d27
v.konovalov <v.konovalov@iskrauraltel.ru>
2023-12-01 08:48:09 +0500

    ДЗ-10 Использование Hibernate. Заготовка

---
+++ hw10-hibernate/src/main/java/ru/otus/hw10/crm/model/Address.java
@@ -0,0 +1,7 @@
+package ru.otus.hw10.crm.model;
+
+public class Address {
+    public Address(Long id, String street) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ hw10-hibernate/src/main/java/ru/otus/hw10/crm/service/DbServiceClientImpl.java
@@ -0,0 +1,58 @@
+package ru.otus.hw10.crm.service;
+
+import java.util.List;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.hw10.core.repository.DataTemplate;
+import ru.otus.hw10.core.sessionmanager.TransactionManager;
+import ru.otus.hw10.crm.model.Client;
+
+public class DbServiceClientImpl implements DBServiceClient {
+    private static final Logger log = LoggerFactory.getLogger(DbServiceClientImpl.class);
+
+    private final DataTemplate<Client> clientDataTemplate;
+    private final TransactionManager transactionManager;
+
+    public DbServiceClientImpl(
+            TransactionManager transactionManager, DataTemplate<Client> clientDataTemplate) {
+        this.transactionManager = transactionManager;
+        this.clientDataTemplate = clientDataTemplate;
+    }
+
+    @Override
+    public Client saveClient(Client client) {
+        return transactionManager.doInTransaction(
+                session -> {
+                    var clientCloned = client.clone();
+                    if (client.getId() == null) {
+                        var savedClient = clientDataTemplate.insert(session, clientCloned);
+                        log.info("created client: {}", clientCloned);
+                        return savedClient;
+                    }
+                    var savedClient = clientDataTemplate.update(session, clientCloned);
+                    log.info("updated client: {}", savedClient);
+                    return savedClient;
+                });
+    }
+
+    @Override
+    public Optional<Client> getClient(long id) {
+        return transactionManager.doInReadOnlyTransaction(
+                session -> {
+                    var clientOptional = clientDataTemplate.findById(session, id);
+                    log.info("client: {}", clientOptional);
+                    return clientOptional;
+                });
+    }
+
+    @Override
+    public List<Client> findAll() {
+        return transactionManager.doInReadOnlyTransaction(
+                session -> {
+                    var clientList = clientDataTemplate.findAll(session);
+                    log.info("clientList:{}", clientList);
+                    return clientList;
+                });
+    }
+}
