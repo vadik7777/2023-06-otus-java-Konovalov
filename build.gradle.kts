7e68f8437d1e77fd8f7accfb9545998f752f89a0
v.konovalov <v.konovalov@iskrauraltel.ru>
2023-12-13 08:06:39 +0500

    ДЗ-12 Веб сервер. Заготовка

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,8 @@ allprojects {
     }
 
     val guava: String by project
+    val jetty: String by project
+    val freemarker: String by project
 
 
     apply(plugin = "io.spring.dependency-management")
@@ -39,6 +41,14 @@ allprojects {
                 mavenBom(BOM_COORDINATES)
             }
             dependency("com.google.guava:guava:$guava")
+            dependency("org.eclipse.jetty:jetty-servlet:$jetty")
+            dependency("org.eclipse.jetty:jetty-server:$jetty")
+            dependency("org.eclipse.jetty:jetty-webapp:$jetty")
+            dependency("org.eclipse.jetty:jetty-security:$jetty")
+            dependency("org.eclipse.jetty:jetty-http:$jetty")
+            dependency("org.eclipse.jetty:jetty-io:$jetty")
+            dependency("org.eclipse.jetty:jetty-util:$jetty")
+            dependency("org.freemarker:freemarker:$freemarker")
         }
     }
     configurations.all {
